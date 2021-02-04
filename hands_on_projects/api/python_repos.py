af0f1351fba4006b04226f6b30d7d2287f066eb4
Daniel Jiang <danieljc8888@gmail.com>
2021-02-04 11:20:34 -0500

    improved paygal vis and customized description tool

---
+++ hands_on_projects/api/python_repos.py
@@ -0,0 +1,51 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# run API and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# store API response in a var
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+# explore the repo info
+repo_dicts = response_dict['items']
+
+print("Repositories returned:", len(repo_dicts))
+
+# # explore the frist repo
+# repo_dict = repo_dicts[0]
+# # print("\nKeys:", len(repo_dict))
+# # for key in sorted(repo_dict.keys()):
+# #     print(key)
+
+names, stars = [], []
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# print result
+# print(response_dict.keys())
+
+# visualiztaion - config
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
